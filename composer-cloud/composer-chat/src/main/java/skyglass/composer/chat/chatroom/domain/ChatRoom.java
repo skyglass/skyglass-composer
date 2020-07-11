93e46889db66c1aaed06609cee0e7cff9590e576
skyglass <michaelsw@ua.fm>
2020-07-11 06:39:28 +0200

    SKC-002
    - upgraded guava library

--- composer-cloud/composer-chat/src/main/java/skyglass/composer/chat/chatroom/domain/ChatRoom.java
+++ composer-cloud/composer-chat/src/main/java/skyglass/composer/chat/chatroom/domain/ChatRoom.java
@@ -6,22 +6,25 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
-//import com.google.common.annotations.VisibleForTesting;
+import com.google.common.annotations.VisibleForTesting;
 
 @RedisHash("chatrooms")
 public class ChatRoom {
-	
+
 	@Id
 	private String id;
+
 	private String name;
+
 	private String description;
+
 	private List<ChatRoomUser> connectedUsers = new ArrayList<>();
-	
+
 	public ChatRoom() {
-	
+
 	}
-	
-	//@VisibleForTesting
+
+	@VisibleForTesting
 	public ChatRoom(String id, String name, String description) {
 		this.id = id;
 		this.name = name;
@@ -31,31 +34,40 @@ public class ChatRoom {
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public List<ChatRoomUser> getConnectedUsers() {
 		return connectedUsers;
 	}
+
 	public void addUser(ChatRoomUser user) {
 		this.connectedUsers.add(user);
 	}
+
 	public void removeUser(ChatRoomUser user) {
 		this.connectedUsers.remove(user);
 	}
-	public int getNumberOfConnectedUsers(){
+
+	public int getNumberOfConnectedUsers() {
 		return this.connectedUsers.size();
 	}
 
