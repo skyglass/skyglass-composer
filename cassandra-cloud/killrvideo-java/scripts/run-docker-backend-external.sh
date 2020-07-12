43916314d9be5b16f0fbf09f90d013d7c635f5dd
skyglass <skyglass2001@gmail.com>
2020-07-12 05:17:11 +0200

    SKC-004
    - small change

--- cassandra-cloud/killrvideo-java/scripts/run-docker-backend-external.sh
+++ cassandra-cloud/killrvideo-java/scripts/run-docker-backend-external.sh
@@ -6,7 +6,7 @@
 #export KILLRVIDEO_BACKEND='docker run --rm alpine nslookup host.docker.internal > ip.log 2>/dev/null && KILLRVIDEO_BACKEND=`tail -1 ip.log | cut -b 12-30` && rm ip.log && echo "IP for Backend is set to : ${KILLRVIDEO_BACKEND}"'
 #export KILLRVIDEO_BACKEND=`docker run --rm alpine nslookup host.docker.internal 2>/dev/null | cut -b 12-28 | tail -1`
 export KILLRVIDEO_BACKEND=192.168.0.59
-export KILLRVIDEO_YOUTUBE_API_KEY=AIzaSyCuvspiQtqIIwSy3eiYAkfiQsrCy-XpBvQ
+export KILLRVIDEO_YOUTUBE_API_KEY=secret
 
 # the compose file swaps in the value of `KILLRVIDEO_BACKEND` in several places
 docker-compose -p killrvideo-java -f docker-compose-backend-external.yaml up -d
--- cassandra-cloud/killrvideo-java/scripts/stop-docker-backend-external.sh
+++ cassandra-cloud/killrvideo-java/scripts/stop-docker-backend-external.sh
@@ -4,7 +4,7 @@
 # `run-docker-backend-external.sh`
 
 export KILLRVIDEO_BACKEND=192.168.0.59
-export KILLRVIDEO_YOUTUBE_API_KEY=AIzaSyCuvspiQtqIIwSy3eiYAkfiQsrCy-XpBvQ
+export KILLRVIDEO_YOUTUBE_API_KEY=secret
 
 docker-compose -f docker-compose-backend-external.yaml down
 
